0998de3b95578e4e0d8dbe7cefe1f9099678315e
ruizi <ruizi@kth.se>
2019-02-26 12:58:08 +0100

    menutype edit ing

--- src/Ingredientslist/Ingredientslist.js
+++ src/Ingredientslist/Ingredientslist.js
@@ -8,7 +8,7 @@ class IngredientsList extends Component {
         this.state={
             dish:this.props.dish,
             menuType:['main course', 'side dish', 'dessert', 'appetizer', 'salad', 'bread', 'breakfast', 'soup', 'beverage', 'sauce', 'drink','undefined'],
-            ordermenu:[{}]
+            ordermenu:[]
          }
          this.addDishToMenu=this.addDishToMenu.bind(this)
     }
@@ -19,11 +19,15 @@ class IngredientsList extends Component {
         do {
             key++;
         } while ((this.state.menuType.indexOf(this.state.dish.dishTypes[key]) == -1) && ((key + 1) < this.state.dish.dishTypes.length));
-        const newOrdermenu = this.state.ordermenu.slice(0); // copy
-        newOrdermenu.splice(this.state.ordermenu.indexOf(this.state.dish.dishTypes[key]),0,this.state.dish);
+        var newOrdermenu= this.state.ordermenu
+        newOrdermenu.splice(this.state.menuType.indexOf(this.state.dish.dishTypes[key]),0,this.state.dish);
+        console.log(key)
+        console.log(this.state.dish.dishTypes[key])
+        console.log(this.state.menuType.indexOf(this.state.dish.dishTypes[key]))
         this.setState({
              ordermenu: newOrdermenu
         });
+        console.log(newOrdermenu)
     }
 
     
